54e1189eb1fd5d80add51c386b7b9f99b4c2f1b2
YasminMuntaserN <161508263+YasminMuntaserN@users.noreply.github.com>
2024-11-20 16:47:23 +0200

    feat: add the setup files

---
+++ linq/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LINQ.Exercises
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            //  Q1_Q10.questions();
+            //  Q10_Q20.questions();
+            // Q20_Q30.questions();
+            //Q30_Q40.questions();
+            Q40_Q50.questions();
+
+
+
+
+        }
+    }
+}
