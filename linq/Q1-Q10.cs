c4ca2bc5c1e778f33b19dac6c5de71e69f665658
YasminMuntaserN <161508263+YasminMuntaserN@users.noreply.github.com>
2024-11-20 16:47:44 +0200

    feat: add question from 1 to 10

---
+++ linq/Q1-Q10.cs
@@ -0,0 +1,144 @@
+﻿using Microsoft.VisualBasic;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Reflection.Metadata.BlobBuilder;
+using System.Xml.Linq;
+using LINQ.Exercises.data.LINQTut07.Shared;
+
+namespace LINQ.Exercises
+{
+    public class Q1_Q10
+    {
+        public static void questions()
+        {
+            // EvenNumbers();
+            // CointainA();
+            //NullOrEmpty();
+            //CitiesWithC();
+            //customersWithCity();
+            // youngestCustomer();
+            //SortCustomer();
+            //SortCustomerByCity();
+            //CustomerByAge();
+            AverageAge();
+        }
+
+        //q1 :Retrieve all even numbers from a list of integers.
+        public static void EvenNumbers()
+        {
+            var numbers = new List<int> { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+            var evenNum = numbers.Where(x => x % 2 == 0).ToList();
+
+            Console.WriteLine(string.Join(" , " , evenNum));
+        }
+
+
+
+        //q2 :Find all strings in a list that contain the letter 'a'.
+        public static void CointainA()
+        {
+            var words = new List<string> { "apple", "banana", "cherry", "date", "fig" };
+
+            var withA = words.Where(x => x.Contains("a")).ToList();
+
+            Console.WriteLine(string.Join(" , ", withA));
+        }
+
+
+        //q3 : Filter out null or empty strings from a list of names.
+        public static void NullOrEmpty()
+        {
+            var names = new List<string> { "John", "", "Jane", null, "Paul" };
+
+
+            var notNul = names.Where(x => !string.IsNullOrEmpty(x)).ToList();
+
+            Console.WriteLine(string.Join(" , ", notNul));
+        }
+        //q4 :Get all cities from a list where the name starts with 'C'.
+        public static void CitiesWithC()
+        {
+            var cities = new List<string> { "Chicago", "New York", "Cleveland", "Dallas" };
+
+            var citiesWithc = cities.Where(x => x.StartsWith("C")).ToList();
+
+            Console.WriteLine(string.Join(" , ", citiesWithc));
+        }
+
+        //q5 :Group customers by their city and count how many customers are in each city.
+
+        public static void customersWithCity()
+        {
+            var customers = Repository.LoadCustomers();
+
+            var CustomersInCity = customers.GroupBy(x => x.City).
+                                  Select(g => new { City = g.Key, Count = g.Count() }).ToList();
+
+
+            foreach (var count in CustomersInCity)
+            {
+                Console.WriteLine($"city : {count.City}  ,Count : {count.Count}");
+            }
+        }
+
+        //q6: Find the youngest customer from each city.
+
+        public static void youngestCustomer()
+        {
+            var customers = Repository.LoadCustomers();
+            var CustomersInCity = customers.GroupBy(x => x.City).
+                               Select(y => new { City = y.Key,
+                                                 Age = y.OrderBy(x => x.Age).First().Age }).ToList();
+            foreach (var count in CustomersInCity)
+            {
+                Console.WriteLine($"city : {count.City}  ,Age : {count.Age}");
+            }
+        }
+
+        //q7:  Sort the customers alphabetically by their names.
+        public static void SortCustomer()
+        {
+            var customers = Repository.LoadCustomers();
+            var CustomersInCity = customers.OrderBy(x=>x.Name);
+            foreach (var customer in CustomersInCity)
+            {
+                Console.WriteLine($"city : {customer.Name}");
+            }
+        }
+
+        //q8: Find customers from Chicago or New York.
+        public static void SortCustomerByCity()
+        {
+            var customers = Repository.LoadCustomers();
+            var CustomersInCity = customers.Where(x=>x.City.Equals("Chicago") || x.City.Equals("New York") ).ToList();
+            foreach (var customer in CustomersInCity)
+            {
+                Console.WriteLine($"city : {customer.City} ,Name : {customer.Name}");
+            }
+        }
+
+        //q9:Get the  customers who are older than 30 and live in Chicago.
+        public static void CustomerByAge()
+        {
+            var customers = Repository.LoadCustomers();
+            var CustomersInCity = customers.Where(x => x.City.Equals("Chicago") && x.Age>30).ToList();
+            foreach (var customer in CustomersInCity)
+            {
+                Console.WriteLine($"city : {customer.City} ,Name : {customer.Name},Age : {customer.Age}");
+            }
+        }
+
+        //q10: Get the average age of customers from New York.
+        public static void AverageAge()
+        {
+            var customers = Repository.LoadCustomers();
+            var avg = customers.Where(x => x.City.Equals("New York")).Average(x => x.Age);
+
+                Console.WriteLine($"average age of customers from New York : {avg}");
+        }
+    }
+}
---
+++ linq/Q10-Q20.cs
@@ -0,0 +1,192 @@
+﻿using LINQ.Exercises.data;
+using LINQ.Exercises.data.LINQTut07.Shared;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace LINQ.Exercises
+{
+    public class Q10_Q20
+    {
+        public static void questions()
+        {
+            // FilterCustomers();
+            // HighestAge();
+            // NamesLengthMoreThan4();
+            // OldestCustomers();
+            //CustomersWIthDictionary();
+            // CustomersWIthHighestAge();
+            //MergeLists1();
+            //NewYorktCustomers();
+            //GroupCustomersWithAge();
+            NewYorktCustomersOrderd();
+
+        }
+
+        //q11:Find customers who live in either 'Chicago' or 'San Francisco' and are older than 30.
+        public static void FilterCustomers()
+        {
+            var customers = Repository.LoadCustomers();
+            var SpesifcCustomers = customers.Where(x => (x.City.Equals("Chicago") || x.City.Equals("San Francisco"))
+                                      && x.Age > 30);
+
+            foreach (var customer in SpesifcCustomers)
+            {
+                Console.WriteLine($"city : {customer.City} ,Name : {customer.Name},Age : {customer.Age}");
+            }
+        }
+
+        //q12: Find the customer with the highest age.
+        public static void HighestAge()
+        {
+            var customers = Repository.LoadCustomers();
+            var highestAge = customers.OrderByDescending(x=>x.Age).First();
+            Console.WriteLine($"customer with the highest age. : Name : {highestAge.Name},Age : {highestAge.Age}");
+        }
+
+        //q13 : Find customers whose names have more than 4 letters.
+        public static void NamesLengthMoreThan4()
+        {
+            var customers = Repository.LoadCustomers();
+            var SpesifcCustomers = customers.Where(x => x.Name.Length >4);
+
+            foreach (var customer in SpesifcCustomers)
+            {
+                Console.WriteLine($"Name : {customer.Name},names letters : {customer.Name.Length}");
+            }
+        }
+
+        //q14: Get the top 3 oldest customers who live in Chicago, sorted by age in descending order.
+        public static void OldestCustomers()
+        {
+            var customers = Repository.LoadCustomers();
+            var SpesifcCustomers = customers.Where(x => x.City.Equals("Chicago")).
+                                    OrderByDescending(x=>x.Age).Take(3).ToList() ;
+
+            foreach (var customer in SpesifcCustomers)
+            {
+                Console.WriteLine($"Name : {customer.Name},names letters : {customer.Age}");
+            }
+        }
+
+        //q15:Create a dictionary where the key is the first letter of the city and the value is the average age of customers from that city.
+        public static void CustomersWIthDictionary()
+        {
+            var customers = Repository.LoadCustomers();
+            var SpesifcCustomers = customers.GroupBy(c => c.City[0])
+                          .ToDictionary(g => g.Key, g => g.Average(c => c.Age)); 
+
+            foreach (var kvp in SpesifcCustomers)
+            {
+                Console.WriteLine($"Name : {kvp.Key},names letters : {kvp.Value}");
+            }
+        }
+
+        //q16:  Find the customers who have the highest age from each city.
+        public static void CustomersWIthHighestAge()
+        {
+            var customers = Repository.LoadCustomers();
+            var oldestPerCity = customers.GroupBy(c => c.City).
+                Select(x=>new { City = x.Key, HighestAge = x.OrderByDescending(a => a.Age).First() }).ToList() ;
+
+            foreach (var customer in oldestPerCity)
+            {
+                Console.WriteLine($"city : {customer.City},HighestAge: {customer.HighestAge}");
+            }
+        }
+
+        //q17:Merge two lists of customers (one with a different dataset) and eliminate duplicates based on name and city.
+        public static void MergeLists1()
+        {
+            var customers1 = Repository.LoadCustomers().Randomize(10);
+            var customers2 = Repository.LoadCustomers().Randomize(10);
+
+            var grouped = customers1.Concat(customers2).DistinctBy(c => new { c.Name, c.City })
+                .GroupBy(x=>x.City).Select(s=>new { City = s.Key, Names = s.Select(x => x.Name).ToList()});
+           
+            foreach (var group in grouped)
+            {
+                Console.WriteLine($"City: {group.City}");
+                foreach (var name in group.Names)
+                {
+                    Console.WriteLine($"   Name: {name}");
+                }
+            }
+
+            //another Soluation
+            var grouped1 = customers1.Union(customers2, new CustomerComparer()).ToList();
+
+            foreach (var group in grouped1)
+            {
+                Console.WriteLine($"City: {group.City} Name: {group.Name}");
+            }
+        }
+
+
+        //q18:Check if all customers in New York are older than 25.
+        public static void NewYorktCustomers()
+        {
+            var customers = Repository.LoadCustomers();
+            var SpesifcCustomers = customers.Where(x => x.City.Equals("New York")).All(x=>x.Age >25);
+
+                Console.WriteLine($"all customers in New York are older than 25. :{SpesifcCustomers}");
+        }
+
+
+        //q19: Group customers by age ranges (e.g., 20-29, 30-39, etc.) and count how many customers are in each age range.
+        public static void GroupCustomersWithAge()
+        {
+            var customers = Repository.LoadCustomers();
+            var groupedByAge = customers.GroupBy(c =>
+            {
+                if (c.Age >= 20 && c.Age <= 29) return "20-29";
+                if (c.Age >= 30 && c.Age <= 39) return "30-39";
+                if (c.Age >= 40 && c.Age <= 49) return "40-49";
+                if (c.Age >= 50 && c.Age <= 59) return "50-59";
+                return "60+";
+            }).Select(x=>new {Range= x.Key , Count =x.Count() }).ToList();
+
+
+            foreach (var group in groupedByAge)
+            {
+                Console.WriteLine($"Age Range: {group.Range}, Count: {group.Count}");
+            }
+
+            // another soluation 
+            var ageRanges = customers
+                 .GroupBy(c => (c.Age / 10) * 10)
+                 .Select(g => new
+                 {
+                     AgeRange = $"{g.Key}-{g.Key + 9}",
+                     Count = g.Count()
+                 })
+                 .ToList();
+           
+            foreach (var group in ageRanges)
+            {
+                Console.WriteLine($"Age Range: {group.AgeRange}, Count: {group.Count}");
+            }
+
+        }
+
+
+
+        // q10: Get customers who live in either New York or Chicago, and order them by age and then by name alphabetically.
+        public static void NewYorktCustomersOrderd()
+        {
+            var customers = Repository.LoadCustomers();
+            var SpesifcCustomers = customers.Where(x => x.City.Equals("New York")).OrderBy(x => x.Age).OrderBy(x=>x.Name);
+
+
+            foreach (var group in SpesifcCustomers)
+            {
+                Console.WriteLine($"City: {group.City} , Age: {group.Age} , Name: {group.Name}");
+            }
+        }
+
+
+    }
+}
